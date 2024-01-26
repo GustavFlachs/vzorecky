781faa4b2ddc0e13ec84237e6c2d8cf0c4ccbf1e
GustavFlachs <152276245+GustavFlachs@users.noreply.github.com>
2024-01-26 08:41:20 +0100

    Add files via upload

---
+++ vše dohromady.py
@@ -0,0 +1,39 @@
+print("Vítejte v aplikaci pro výpočet vzorečků pro obdelník")
+
+print("Pro výpočet objemu zadejte 1.")
+print("Pro výpočet obvodu zadejte 2.")
+print("Pro výpočet obsahu zadejte 3.")
+print("Pro ukončení zadejte 4")
+
+volba = input("Zadejte vaší volbu: ")
+
+if volba == "1":
+    a = int(input("Zadejte stranu a: "))
+    b = int(input("Zadejte stranu b: "))
+    c = int(input("Zadejte stranu c: "))
+
+    if a>0 and b>0 and c>0:
+        výsledek = a*b*c
+
+        print("objem kvádru je :, výsledek")
+    else:
+        print("co to robíš chuligáne?")
+
+if volba == "2":
+    a = int(input("Zadejte stranu a: "))
+    b = int(input("Zadejte stranu b: "))
+    if a>0 and b>0:
+        print("Výsledek je: ", 2*a+2*b)
+    else:
+        print("NEEEE!")
+
+if volba == "3:":
+    a = int(input("Zadejte stranu a: "))
+    b = int(input("Zadejte stranu b: "))
+    if a>0 and b>0:
+        print("Výsledek je: ", a*b)
+    else:
+        print("NEEEEEEEEEEE!")
+
+otazka = input("chcete vypočítat nečo dalšího? Jestli ne napište 4 jestli ano napište 5.")
+if otazka == "4":
